8197779d01e540231e97e4cde5c205962fcde080
joyalshaji135 <joyalshaji135@gmail.com>
2024-04-01 21:44:23 +0530

    Biggest

--- MCA-Lab-Questions/Biggest_Of_Two_Numbers/index.php
+++ MCA-Lab-Questions/Biggest_Of_Two_Numbers/index.php
@@ -8,7 +8,7 @@
 <body>
 <center>   
 <h1>Biggest of two Numbers</h1>
-<form action="" method="post">
+<form action="index.php" method="post">
     <p>
         <label for="number_1">Enter the First Number</label>
         <input type="number" name="number1" id="">
@@ -22,3 +22,26 @@
 </center>
 </body>
 </html>
+
+<?php
+
+if ($_POST) {
+    $number1 = $_POST['number1'];
+    $number2 = $_POST['number2'];
+
+    // if ($number1 > $number2) {
+    //     echo "<center>The Biggest Number is :".$number1."</center>";
+    // }
+    // else
+    // {
+    //     echo "<center>The Smallest Number is :".$number2."</center>";
+    // }
+
+    // Ternary Operation 
+
+    $main = ($number1 > $number2) ? $number1 : $number2;
+  print ("The Biggest Number is :".$main);
+}
+
+
+?>
