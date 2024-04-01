464cced1af72f66269544cbd338070653f22a9e5
joyalshaji135 <joyalshaji135@gmail.com>
2024-04-01 21:30:15 +0530

    Basic Question in Php

---
+++ MCA-Lab-Questions/Biggest_Of_Two_Numbers/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biggest Of Two Numbers</title>
+</head>
+<body>
+<center>   
+<h1>Biggest of two Numbers</h1>
+<form action="" method="post">
+    <p>
+        <label for="number_1">Enter the First Number</label>
+        <input type="number" name="number1" id="">
+    </p>
+    <p>
+        <label for="number_2">Enter the Second Number</label>
+        <input type="number" name="number2" id="">
+    </p>
+    <button>Submit</button>
+</form>
+</center>
+</body>
+</html>
