bccfab5027af7fc89ab6816b78874b8f3064a530
joyalshaji135 <joyalshaji135@gmail.com>
2024-04-25 23:43:11 +0530

    Array Problem

--- MCA-Lab-Questions/String_Build_Type_Operation/index.php
+++ MCA-Lab-Questions/String_Build_Type_Operation/index.php
@@ -50,13 +50,13 @@ if ($_POST) {
     echo "<center>
     <table border='1'>";
     echo "<tr><th>String Use Define</th><th>Sting Output</th></tr>";
-    echo "<tr><td>String to Lower :</td><td>".$str1."</td></tr>";
-    echo "<tr><td>String to Upper :</td><td>".$str2."</td></tr>";
-    echo "<tr><td>String Upper Case First :</td><td>".$str3."</td></tr>";
-    echo "<tr><td>String Lower Case First :</td><td>".$str4."</td></tr>";
-    echo "<tr><td>String First Character of each Word into uppercase :</td><td>".$str5."</td></tr>";
-    echo "<tr><td>String Reverse :</td><td>".$str6."</td></tr>";
-    echo "<tr><td>String Length :</td><td>".$str7."</td></tr>";
+    echo "<tr><td>String to Lower'".$word."' :</td><td>".$str1."</td></tr>";
+    echo "<tr><td>String to Upper '".$word."':</td><td>".$str2."</td></tr>";
+    echo "<tr><td>String Upper Case First '".$word."':</td><td>".$str3."</td></tr>";
+    echo "<tr><td>String Lower Case First '".$word."':</td><td>".$str4."</td></tr>";
+    echo "<tr><td>String First Character of each Word into uppercase '".$word."':</td><td>".$str5."</td></tr>";
+    echo "<tr><td>String Reverse '".$word."':</td><td>".$str6."</td></tr>";
+    echo "<tr><td>String Length '".$word."':</td><td>".$str7."</td></tr>";
     echo "</table></center>";
 }
 
